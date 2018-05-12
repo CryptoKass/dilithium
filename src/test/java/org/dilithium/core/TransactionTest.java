0e0d0a99ccfdecdb87184240a52f08fa65bbcd3b
Lev Knoblock <levknoblock@gmail.com>
2018-05-12 03:21:14 +0300

    Added additional portion to Transaction Testing that makes sure an individual cannot sign a transaction on behalf of another.

--- src/test/java/org/dilithium/core/TransactionTest.java
+++ src/test/java/org/dilithium/core/TransactionTest.java
@@ -4,6 +4,7 @@ import org.bouncycastle.jce.provider.BouncyCastleProvider;
 import org.bouncycastle.util.encoders.Hex;
 import org.dilithium.core.axiom.AxiomD0;
 import org.dilithium.db.Context;
+import org.dilithium.util.ByteUtil;
 import org.dilithium.util.Log;
 import org.dilithium.util.ecdsa.ECKey;
 import org.junit.Assert;
@@ -30,7 +31,15 @@ public class TransactionTest {
         Wallet wallet = new Wallet();
         Transaction tx = wallet.generateTransaction(BigInteger.TEN, (new ECKey()).getAddress(), (byte)0x00, new Context(), new AxiomD0());
 
-        Assert.assertTrue("Transaction signature validated correctly.", tx.verifySignature(new AxiomD0()));
+        Assert.assertTrue("Transaction signature was invalid.", tx.verifySignature(new AxiomD0()));
 
+        Log.log(Level.INFO, "Transaction successfully validated.");
+
+        Log.log(Level.INFO, "Now testing to make sure people can't sign transactions on behalf of others.");
+        Transaction tx2 = new Transaction((new ECKey()).getPrivKeyBytes(), ByteUtil.bigIntegerToBytes(BigInteger.ONE), ByteUtil.bigIntegerToBytes(BigInteger.ONE), ByteUtil.bigIntegerToBytes(BigInteger.TEN), (new ECKey()).getAddress(), (byte)0x00, (new ECKey()).getAddress(), new AxiomD0());
+
+        Assert.assertFalse("Malicious transaction successfully validated. This shouldn't happen.", tx2.verifySignature(new AxiomD0()));
+
+        Log.log(Level.INFO, "Malicious transaction failed to validate. This is exactly what should happen.");
     }
 }
